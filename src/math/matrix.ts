49a7490347f4f74bf19ef3cad93276fa0bc04b9c
Kevin Helfrich <khelfrich72@gmail.com>
2017-04-17 15:49:27 +0200

    added more matrix functions

--- src/math/matrix.ts
+++ src/math/matrix.ts
@@ -1,9 +1,9 @@
-import Vector from './Vector';
+import {Vector} from './Vector';
 export class Matrix{
     elements: number[];
 
     constructor(){
-        elements = [0,0,0,
+        this.elements = [0,0,0,
                     0,0,0,
                     0,0,0];
     }
@@ -13,5 +13,38 @@ export class Matrix{
             this.elements[0]*rhs.x + this.elements[1]*rhs.y + this.elements[2]*rhs.z,
             this.elements[3]*rhs.x + this.elements[4]*rhs.y + this.elements[5]*rhs.z,
             this.elements[6]*rhs.x + this.elements[7]*rhs.y + this.elements[8]*rhs.z);
+    }
+
+    multiply(rhs:Matrix):Matrix{
+        var result = new Matrix();
+
+        for(var a = 0;a < 3;a++){
+            for(var b = 0; b < 3; b++){
+                var sum:number = 0;
+                for(var c = 0;c < 3; c++){
+                    sum += this.elements[a+3*c]*rhs.elements[c+3*b];
+                }
+                result.elements[a+3*b] = sum;
+            }
+        }
+        return result;
+    }
+
+    setRotX(angle:number){
+        this.elements = [1,0,0,
+                        0,Math.cos(angle),-1*Math.sin(angle),
+                        0,Math.sin(angle),Math.cos(angle)];
+    }
+
+    setRotY(angle:number){
+        this.elements = [Math.cos(angle),0,Math.sin(angle),
+                        0,1,0,
+                        -1*Math.sin(angle),0,Math.cos(angle)];
+    }
+
+    setRotZ(angle:number){
+        this.elements = [Math.cos(angle),-1*Math.sin(angle),0,
+                        Math.sin(angle),Math.cos(angle),0,
+                        0,0,1];
     }
 }
