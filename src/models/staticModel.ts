e3f3c72251b92d139c471c8fd583251e3ad39087
Kevin Helfrich <khelfrich72@gmail.com>
2017-04-17 16:29:32 +0200

    added vooxel and model objects

---
+++ src/models/staticModel.ts
@@ -0,0 +1,9 @@
+import {Vector} from '../math/Vector';
+import {Matrix} from '../math/Matrix';
+import {Voxel} from './Voxel';
+
+export class staticModel{
+    position:Vector;
+    orientation:Vector;
+    voxels:Voxel[];
+}
---
+++ src/models/voxel.ts
@@ -0,0 +1,11 @@
+import {Vector} from '../math/Vector';
+
+export class Voxel{
+    position: Vector;
+    color: string;
+
+    constructor(position:Vector,red:number,green:number,blue:number){
+        this.position = position;
+        this.color = '#' + red.toString(16) + green.toString(16) + blue.toString(16);
+    }
+}
